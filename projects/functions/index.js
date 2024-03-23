c368a44289112c054d1c6b5f10780bdeb03288d0
ModdexEk <moddex@yandex.ru>
2024-03-23 12:22:10 +0500

    the task of the first week

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(value) {
+  return value;
+}
 
 /*
  Задание 2:
@@ -28,7 +30,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(num1, num2 = 100) {
+  return num1 + num2;
+}
 
 /*
  Задание 3:
@@ -38,8 +42,9 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
-
+function returnFnResult(fn) {
+  return fn();
+}
 /*
  Задание 4:
 
@@ -53,8 +58,9 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
-
+function returnCounter(i = 0) {
+  return () => ++i;
+}
 /*
  Задание 5 *:
 
@@ -64,7 +70,9 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...array) {
+  return array;
+}
 
 export {
   returnFirstArgument,
